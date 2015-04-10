5e7221494e58bd60caeb654928020631954e1bee
kevin hu <sky790312yks@gmail.com>
2015-04-10 15:34:46 +0800

    add full image background

--- js/jquery-scroll-animate.js
+++ js/jquery-scroll-animate.js
@@ -50,4 +50,32 @@
       })
 
   }
+
+
+
+  var _object = null;
+  $(window).resize(function() {
+    var _size;
+    if (!screen.deviceXDPI && !screen.logicalXDPI) {
+      _size = window.devicePixelRatio;
+    } else {
+      _size = screen.deviceXDPI / screen.logicalXDPI;
+    }
+    if (_object !== null) {
+      _object.each(function() {
+        $(this).css({
+          "zoom": 1 / _size,
+          "-moz-transform": "scale(" + 1 / _size + ")"
+        });
+      });
+    }
+  }).resize();
+  $.fn.fixedZoom = function() {
+    if (_object === null ) {
+      _object = this;
+    } else {
+      _object.add(this)
+    }
+    return this;
+  };
 })(jQuery);
